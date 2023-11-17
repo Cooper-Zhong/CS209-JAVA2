45ce2e4adb64dca5f2577ef12ee3b66409b2b5ca
Cooper <12110517@mail.sustech.edu.cn>
2023-11-18 00:47:56 +0800

    refactor. pause/resume/cancel ok.

---
+++ java2-Assignment2/src/main/java/org/example/TransferTask.java
@@ -0,0 +1,262 @@
+package org.example;
+
+import java.io.*;
+import java.net.Socket;
+
+public class TransferTask implements Runnable {
+    private static final String UPLOAD_FOLDER = "Upload/";
+
+    private static final String STORAGE_FOLDER = "Storage/";
+
+    private static final String DOWNLOAD_FOLDER = "Download/";
+
+    public String direction; // upload or download
+
+    public Socket socket;
+
+    public DataInputStream dis;
+
+    public DataOutputStream dos;
+
+    public InputStream inputStream;
+
+    public OutputStream outputStream;
+
+    public long currentBytes;
+
+    public long totalBytes;
+
+    public File file;
+
+    public volatile boolean canceled;
+    public volatile boolean isPaused = false;
+
+    public TransferTask(Socket socket, File file, String direction) {
+        this.socket = socket;
+        this.file = file;
+        this.direction = direction;
+        this.currentBytes = 0;
+        this.totalBytes = 0;
+        this.canceled = false;
+        try {
+            this.inputStream = socket.getInputStream();
+            this.outputStream = socket.getOutputStream();
+            this.dis = new DataInputStream(inputStream);
+            this.dos = new DataOutputStream(outputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public TransferTask(Socket socket, String direction) {
+        this.socket = socket;
+        this.direction = direction;
+        this.currentBytes = 0;
+        this.totalBytes = 0;
+        this.canceled = false;
+        try {
+            this.inputStream = socket.getInputStream();
+            this.outputStream = socket.getOutputStream();
+            this.dis = new DataInputStream(inputStream);
+            this.dos = new DataOutputStream(outputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void run() {
+        switch (direction) {
+            case "upload":
+                upload();
+                break;
+            case "download":
+                download();
+                break;
+            case "handleUpload":
+                handleUpload();
+                break;
+            default:
+                System.out.println("Invalid direction.");
+        }
+    }
+
+    private static String getDirectoryName(String fileName) {
+        int lastSeparatorIndex = fileName.lastIndexOf('/');
+        if (lastSeparatorIndex != -1) {
+            return fileName.substring(0, lastSeparatorIndex);
+        }
+        return "";
+    }
+
+    private void download() {
+        try {
+            String fileName = file.getPath();
+            // 检查并创建文件夹
+            File directory = new File(DOWNLOAD_FOLDER + getDirectoryName(fileName));
+            if (!directory.exists()) {
+                directory.mkdirs(); // 递归创建文件夹
+            }
+
+            dos.writeUTF("download");
+            dos.writeUTF(fileName);
+
+            totalBytes = dis.readLong();
+            FileOutputStream fileOutputStream = new FileOutputStream(DOWNLOAD_FOLDER + fileName);
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+
+            label:
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+
+                while (isPaused) {
+                    if (canceled) break label;
+                    try {
+                        Thread.sleep(100); // Sleep for a short time and check again
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                if (canceled) break;
+                fileOutputStream.write(buffer, 0, bytesRead);
+                currentBytes += bytesRead;
+            }
+
+            fileOutputStream.close();
+
+            if (canceled) {
+                File file = new File(DOWNLOAD_FOLDER + fileName);
+                file.delete(); // Delete the file if the download is canceled
+                System.out.println("====================================");
+                System.out.println("Download canceled: " + fileName);
+                System.out.println("====================================");
+            } else {
+                System.out.println("====================================");
+                System.out.println("File downloaded: " + fileName);
+                System.out.println("====================================");
+            }
+
+            dis.close();
+            socket.close();
+            newClient.TaskMap.remove(file.getName());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void upload() {
+        try {
+            dos.writeUTF("upload");
+            String fileName = file.getPath().substring(UPLOAD_FOLDER.length());
+
+            dos.writeUTF(fileName); //fileName with relative path
+            dos.writeLong(file.length()); //size
+            totalBytes = file.length();
+
+            // Create input stream to read the file
+            FileInputStream fis = new FileInputStream(file);
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+
+            // Send file content to the server
+            label:
+            while ((bytesRead = fis.read(buffer)) != -1) {
+                while (isPaused) {
+                    if (canceled) break label;
+                    try {
+                        Thread.sleep(100); // Sleep for a short time and check again
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (canceled) break;
+                outputStream.write(buffer, 0, bytesRead);
+                currentBytes += bytesRead;
+            }
+
+            if (canceled) {
+                System.out.println("====================================");
+                System.out.println("Upload canceled: " + file.getName());
+                System.out.println("====================================");
+            }
+
+            try {
+                Thread.sleep(100); // Sleep for a short time and check again
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // Signal the end of file
+            fis.close();
+            dos.close();
+            socket.close();
+            newClient.TaskMap.remove(file.getName());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleUpload() {
+        try {
+            // Read file name and size from the client
+            String fileName = dis.readUTF();
+
+            // 检查并创建文件夹
+            File directory = new File(STORAGE_FOLDER + getDirectoryName(fileName));
+            if (!directory.exists()) {
+                directory.mkdirs(); // 递归创建文件夹
+            }
+
+            long fileSize = dis.readLong();
+
+            // Set up output stream to write the file
+            FileOutputStream fileOutputStream = new FileOutputStream(STORAGE_FOLDER + fileName);
+            byte[] buffer = new byte[4096];
+            int bytesRead = 0;
+
+            // Receive file content from the client
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                fileOutputStream.write(buffer, 0, bytesRead);
+                if (canceled) break; // 检查是否取消
+            }
+
+            fileOutputStream.close();
+            if (!canceled) {
+                System.out.println("====================================");
+                System.out.println("File received: " + fileName);
+                System.out.println("====================================");
+            } else {
+                File file = new File(STORAGE_FOLDER + fileName);
+                file.delete();
+                System.out.println(fileName + " canceled by the client.");
+            }
+
+            dis.close();
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 设置暂停状态
+    public void pause() {
+        isPaused = true;
+    }
+
+    // 恢复任务执行
+    public void resume() {
+        isPaused = false;
+    }
+
+    // 取消任务执行
+    public void cancel() {
+        canceled = true;
+    }
+
+
+}
---
+++ java2-Assignment2/src/main/java/org/example/newClient.java
@@ -0,0 +1,228 @@
+package org.example;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.concurrent.*;
+
+public class newClient {
+
+    private static final String SERVER_IP = "localhost";
+    private static final int SERVER_PORT = 12345;
+    private static final String UPLOAD_FOLDER = "Upload/";
+    private static final String DOWNLOAD_FOLDER = "Download/";
+    static Map<String, Future<?>> futures = new ConcurrentHashMap<>();
+    static ExecutorService executor = Executors.newFixedThreadPool(5);
+    public static Map<String, TransferTask> TaskMap = new ConcurrentHashMap<>();
+
+
+    public static void main(String[] args) {
+        try {
+            // main socket
+            Socket socket = new Socket(SERVER_IP, SERVER_PORT);
+            OutputStream outputStream = socket.getOutputStream();
+            DataOutputStream dos = new DataOutputStream(outputStream);
+            InputStream inputStream = socket.getInputStream();
+            DataInputStream dis = new DataInputStream(inputStream);
+            PrintWriter out = new PrintWriter(outputStream, true);
+
+            System.out.println("Connected to server " + SERVER_IP + ":" + SERVER_PORT);
+            dos.writeUTF("main");
+
+            Scanner sc = new Scanner(System.in);
+
+            label:
+            while (true) {
+                System.out.println("1 : Upload");
+                System.out.println("2 : Download");
+                System.out.println("3 : Query progress");
+                System.out.println("4 : pause");
+                System.out.println("5 : resume");
+                System.out.println("6 : cancel");
+                System.out.println("7 : exit");
+                System.out.println("Enter your choice: ");
+                String choice = sc.nextLine();
+                System.out.println("====================================");
+                switch (choice) {
+                    case "1":
+                        uploadFolder(UPLOAD_FOLDER);
+                        break;
+                    case "2":
+                        queryResources(dis, dos);
+                        System.out.print("Enter the file/folder name to download: ");
+                        downloadFolder(sc.nextLine(), dis, dos);
+                        break;
+                    case "3":
+                        queryTasks(); // Query all tasks and their progress
+                        break;
+                    case "4":
+                        System.out.println("Enter the file name: ");
+                        pauseTask(sc.nextLine());
+                        break;
+                    case "5":
+                        System.out.println("Enter the file name: ");
+                        resumeTask(sc.nextLine());
+                        break;
+                    case "6":
+                        System.out.println("Enter the file name: ");
+                        cancelTask(sc.nextLine());
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case "7":
+                        dos.writeUTF("exit");
+                        out.println("exit");
+                        break label;
+                    default:
+                        System.out.println("Invalid choice.");
+                }
+            }
+
+            out.close();
+            socket.close();
+            System.out.println("Disconnected from server.");
+            executor.shutdown();
+            System.exit(0);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void queryResources(DataInputStream dis, DataOutputStream dos) {
+        try {
+            dos.writeUTF("query");
+            String fileName;
+            while (!(fileName = dis.readUTF()).equals("END_OF_LIST")) {
+                System.out.print(fileName + "\t");
+            }
+            System.out.println();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void cancelTask(String name) {
+        TransferTask task = TaskMap.get(name);
+        if (task == null) {
+            System.out.println("No such file.");
+            return;
+        }
+        task.cancel();
+    }
+
+    private static void resumeTask(String name) {
+        TransferTask task = TaskMap.get(name);
+        if (task == null) {
+            System.out.println("No such file.");
+            return;
+        }
+        task.resume();
+    }
+
+    private static void pauseTask(String name) {
+        TransferTask task = TaskMap.get(name);
+        if (task == null) {
+            System.out.println("No such file.");
+            return;
+        }
+        task.pause();
+    }
+
+    private static void uploadFolder(String folderName) {
+        File folder = new File(folderName);
+        File[] files = folder.listFiles();
+        if (files != null && files.length>0) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    uploadFolder(folderName + file.getName() + "/");
+                } else {
+                    try {
+                        Socket socket = new Socket(SERVER_IP, SERVER_PORT);
+                        TransferTask task = new TransferTask(socket, file, "upload");
+                        TaskMap.put(file.getName(), task);
+                        Future<?> future = executor.submit(task);
+                        futures.put(file.getName(), future);
+
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        } else {
+            System.out.println("No files found in the folder: " + folder.getName());
+        }
+    }
+
+
+    private static void downloadFolder(String fileName, DataInputStream dis, DataOutputStream dos) {
+        List<String> files = askName(fileName, dis, dos); // 获得文件夹下的所有文件名.
+        for (String file : files) {
+            try {
+                Socket socket = new Socket(SERVER_IP, SERVER_PORT);
+                TransferTask task = new TransferTask(socket, new File(file), "download");
+                file = getFileName(file);
+                TaskMap.put(file, task);
+                Future<?> future = executor.submit(task);
+                futures.put(file, future);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static String getFileName(String filePath) {
+        int lastSeparatorIndex = filePath.lastIndexOf('/');
+        if (lastSeparatorIndex >= 0 && lastSeparatorIndex < filePath.length() - 1) {
+            return filePath.substring(lastSeparatorIndex + 1);
+        }
+        return filePath; // 如果字符串不包含斜杠，则直接返回原始字符串
+    }
+
+    private static List<String> askName(String fileName, DataInputStream dis, DataOutputStream dos) {
+        // 获得一个文件到底是文件还是文件夹，如果是文件夹，拿到文件夹下的所有文件名
+        try {
+            dos.writeUTF("ask");
+            dos.writeUTF(fileName);
+            ArrayList<String> names = new ArrayList<>();
+            String name;
+            while (!(name = dis.readUTF()).equals("END_OF_LIST")) {
+                System.out.print(name + "\t");
+                names.add(name);
+            }
+            System.out.println();
+            return names;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static void queryTasks() {
+        System.out.println("====================================");
+        System.out.println("Current tasks:");
+        if (TaskMap.isEmpty()) {
+            System.out.println("No tasks.");
+            System.out.println("============");
+            return;
+        }
+        for (Map.Entry<String, TransferTask> entry : TaskMap.entrySet()) {
+            String fileName = entry.getKey();
+            TransferTask task = entry.getValue();
+            String status = task.isPaused ? "Paused" : "In Progress";
+            long current = task.currentBytes;
+            long total = task.totalBytes;
+            System.out.print(task.direction + ": ");
+            System.out.print(fileName + ": " + (current/1024) + "/" + (total/1024) + " KB\t");
+            System.out.printf("%.2f", (current*100.0/total));
+            System.out.println("%\t" + status);
+            System.out.println("====================================");
+        }
+    }
+}
